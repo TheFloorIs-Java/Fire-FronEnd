153b76f0cdfc92ab0bc574968f4cdcd5838ac942
laalsanedo <rpatel1320@yahoo.com>
2022-10-17 20:49:59 -0500

    changed production environment setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  withCredentials: true,
+  baseUrl: "http://localhost:8080",
+  // baseUrl: "https://fire-shop.azurewebsites.net",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+  },
 };

f58c8edc1c384cf1c48e0af2bb13ddd04a9daf12
Eugene <eugenehkim25@gmail.com>
2022-10-21 14:34:43 -0600

    Update product details with mat card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import {MatSortModule} from "@angular/material/sort";
 import {MatInputModule} from "@angular/material/input";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from '@angular/material/card'; 
 
 
 @NgModule({
@@ -63,7 +64,8 @@ import {MatButtonModule} from "@angular/material/button";
         MatSortModule,
         MatInputModule,
         MatPaginatorModule,
-        MatButtonModule
+        MatButtonModule,
+        MatCardModule
     ],
   providers: [],
   bootstrap: [AppComponent]

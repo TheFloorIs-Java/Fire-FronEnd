fa36760f8a04400d2de6e8d7df9a5fd08e2210a1
abdelwahab <abdelwahab362@Revature.net>
2022-10-24 11:35:41 -0500

    documentation

--- src/app/services/review.service.ts
+++ src/app/services/review.service.ts
@@ -11,35 +11,47 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class ReviewService {
-   product !: Product ;
-  //  reviewrs !: Review;
+   //create product object
+  product !: Product ;
+  // create array for all review
    allReviews : Review[] = [];
+   // inject HttpClient in ReviewService class
+
   constructor(private http: HttpClient) { 
 
   }
-
-  addReview( reviewr:Review): void {
-  // this.http.post<Review>("http://localhost:8080/review/", 
+   // create addReview method which accep Review object and use to add that Review object in database
+ /**
+     * This method is used to add Review object to the  database
+     * @param reviewr This is the first paramter to addReviw method
+     * @return Nothing
+     */
+   addReview( reviewr:Review): void {
+  
   this.http.post<Review>(environment.baseUrl+"/review/", 
    reviewr,{headers: environment.headers, withCredentials: environment.withCredentials}).subscribe(()=>  console.log("review added"));
    
   }
-
-
+  
+   
   getUser():  Observable<User> {
     return this.http.get<User>(environment.baseUrl+"/userInfo/");
   }
-
-  getReview(id: number): Observable<Review[]> {
-    return this.http.get<Review[]>(environment.baseUrl+"/review/"+id);
-  }
+   // create getReview method which pass id parameter and get array of Reviews
+  //getReview(id: number): Observable<Review[]> {
+  //  return this.http.get<Review[]>(environment.baseUrl+"/review/"+id);
+ // }
   
  
-  
+// Define getReview method which pass number parameter and return array of Reviews filtered by product id
+/**
+     * This method is used to get array of  Review object from   database by use product_id
+     * @param id This is the first paramter to addReviw method
+     * @return Review[] This return Review array
+ */
 getAllReviews(id:number ) : Observable<Review[]> {
   return this.http.get<Review[]>(environment.baseUrl+"/review/" +  id);
-  //.subscribe(data=> {this.allReviews =data ; console.log(this.allReviews)});
-  
+    
 }
   
 

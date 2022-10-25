8d71b3f9323cea22968f01f4ac463755b9f7c727
programjourney1 <programjourney1@gmail.com>
2022-10-25 16:26:22 -0400

    testing

--- src/app/models/payment.spec.ts
+++ src/app/models/payment.spec.ts
@@ -2,6 +2,7 @@ import { Payment } from './payment';
 
 describe('Payment', () => {
   it('should create an instance', () => {
-    expect(new Payment()).toBeTruthy();
+    expect(new Payment( 'name',
+      'detail')).toBeTruthy();
   });
 });
--- src/app/models/review.model.spec.ts
+++ src/app/models/review.model.spec.ts
@@ -1,7 +1,8 @@
+import { Product } from './product';
 import { Review } from './review';
 
 describe('Review', () => {
   it('should create an instance', () => {
-    expect(new Review()).toBeTruthy();
+    expect(new Review(new Product(1, 'name', 1, 'description', 1, 'image'), 'p')).toBeTruthy();
   });
 });
